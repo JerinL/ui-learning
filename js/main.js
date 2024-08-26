d3eb5e47546f43f51b368babd1e0cabd6d8e82f2
Jerin <jerin.l@aryanow.com>
2024-08-26 16:13:01 +0530

    message

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// let -> used for initialize the myVariable
+// const -> used for tha variable is constant 
+
+console.log("hii broo");
+myVariable = "j e r  i n";
+console.log(myVariable.indexOf("e"));
+console.log(myVariable.split(""));
+console.log(myVariable.slice("r"));
+console.log(myVariable.toUpperCase());
+
+
+myNumber = 121;
+
+console.log(Number(1))
+
+
+const num = 22;
+const num2 = 44;
+const sum1 = num + num2;
+
+console.log(sum1)
+
+console.log(num === num2)
+
+const strNum = "1010";
+console.log(Number(strNum));
+
+
+console.log(Number.isInteger(num2));''
